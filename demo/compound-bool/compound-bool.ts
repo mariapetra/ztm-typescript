eef05d8b3d1ee6399d7391ebe6ac8661f9a795ca
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-12-15 13:08:57 +0800

    compound boolean

--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,3 +1,32 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+//not
+const writing = true;
+const reading = !writing;
+assert.equal(reading, false);
+
+
+//or
+const rating = 9;
+const favouriteMovie = false;
+
+
+const suggestiveMovie = rating > 8 || favouriteMovie;
+assert.equal(suggestiveMovie, true);
+// compund boolean expression (pipes mean or)
+// or is short circuiting once something is true the rest is ignored
+
+//and = all expressions must be true
+const age = 18;
+const isTeen = age >= 13 && age < 20;
+assert.equal(isTeen, true);
+
+const packageWeight = 30
+const packageLength = 50;
+
+const feeExemption = false;
+
+const extraFee = !feeExemption && (packageWeight > 25 || packageLength > 40);
+//when you have more than one and or more than one or we can pack these into individual units
+// it gets too confusing and too easy to make mistakes
